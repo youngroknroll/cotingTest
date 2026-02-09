079bbd171b8cbfe7b0483f178f48f5a85b3aef3c
Yeongrok Song <78721108+youngroknroll@users.noreply.github.com>
2026-02-09 20:49:45 +0900

    [level 2] Title: H-Index, Time: 0.17 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,13 +1,9 @@
 def solution(citations):
-    # 인용 횟수를 내림차순으로 정렬
     citations.sort(reverse=True)
     
-    # 최대 H-Index 계산
-    h_index = 0
     for i in range(len(citations)):
-        if citations[i] >= i + 1:  # 현재 논문이 (i+1)번 이상 인용되었는지 확인
-            h_index = i + 1       # H-Index 갱신
-        else:
-            break                 # 조건을 만족하지 않으면 루프 종료
-    
-    return h_index
+        if citations[i] < i + 1:
+            return i
+        
+    answer = len(citations)
+    return answer
